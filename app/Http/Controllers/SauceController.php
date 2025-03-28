07db08d9ee5ea01665b62b2e2720c0aa90f93dc1
713koukou-naizaa <nzo.akt@gmail.com>
2025-03-28 21:04:49 +0100

    commented out element for web

--- app/Http/Controllers/SauceController.php
+++ app/Http/Controllers/SauceController.php
@@ -10,7 +10,7 @@ class SauceController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request)
+    public function index(/*Request $request*/)
     {
         $sauces = Sauce::all();
         return response()->json($sauces);
