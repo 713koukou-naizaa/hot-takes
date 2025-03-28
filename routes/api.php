5a9cb0e1d68d00cb7441a6746a67b0ab93a9a620
713koukou-naizaa <nzo.akt@gmail.com>
2025-03-28 20:58:41 +0100

    removed all from gitignore

--- routes/api.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\SauceController;
-
-Route::middleware('auth:sanctum')->group(function () {
-    Route::apiResource('sauces', SauceController::class);
-});
